efec538d1f53861b911e7b2d59ce5665ddf0a5ca
Nikky Rojas <rojas7313@gmail.com>
2022-07-08 07:46:07 -0600

    fixed code

--- spec/features/doctors/doctors_show_spec.rb
+++ spec/features/doctors/doctors_show_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-# For each parent table
+# For each child table
 # As a visitor
-# When I visit '/parents'
-# Then I see the name of each parent record in the system
-RSpec.describe 'the songs show the page' do
+# When I visit '/child'
+# Then I see the name of each child record in the system
+RSpec.describe 'the page shows doctor' do
     it 'displays indvidual doctor' do
         hospital = Hospital.create!(name: 'Childrens Hospital', public: true, phone: 6213333, location: 'Aurora')
         doctor = hospital.doctors.create!(name: 'Theodore Seuss', probono: true, specialty: 'Pediatrics')
@@ -26,6 +26,21 @@ RSpec.describe 'the songs show the page' do
         save_and_open_page
 
         expect(page).to have_content(hospital.name)
+    
+    end
+
+    it 'displays the attributes of the doctor' do
+        hospital = Hospital.create!(name: 'Childrens Hospital', public: true, phone: 6213333, location: 'Aurora')
+        doctor = hospital.doctors.create!(name: 'Theodore Seuss', probono: true, specialty: 'Pediatrics')
+        doctor_2 = hospital.doctors.create!(name: 'Gregory House', probono: false, specialty: 'Diagnostic Medicine')
+        
+        visit "/doctors/#{doctor.id}"
+        save_and_open_page
+        
+        expect(page).to have_content(doctor.specialty)
+        expect(page).to have_content(doctor.probono)
+        expect(page).to have_content(doctor.license_num)
+        expect(page).to_not have_content(doctor_2.specialty)
     
     end
 
